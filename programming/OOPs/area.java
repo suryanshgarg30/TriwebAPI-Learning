7b6a33d29f2b3fc1994a8935402c1ef0f9fded29
suryansh garg <suryanshgarg.1130@gmail.com>
2024-05-30 14:45:02 +0530

    added oops and managed repo

---
+++ programming/OOPs/area.java
@@ -0,0 +1,12 @@
+class abc {
+      public static int area(int 1, int b)}
+           return l*b;
+      
+      public static int volume(int l, int b, int h){
+           return l*b*h;
+      }
+public static void main(string[]args){
+     system.out.print1n("Hellow World");
+     system.out.print1n("Area=" +area(5,4));
+     system.out.print1n("Volume="+volume(4,5,6));
+}
